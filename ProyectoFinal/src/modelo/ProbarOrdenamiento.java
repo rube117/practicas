7621535ad863ebdc30b70aed45b72ce2196871da
T-101 <T-101@PC229>
2018-07-26 19:39:30 -0500

    Randomizar orden no repetido

---
+++ ProyectoFinal/src/modelo/ProbarOrdenamiento.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.*;
+
+/**
+ *
+ * @author T-101
+ */
+public class ProbarOrdenamiento {
+    public static void main(String[] args) {
+        // Un arreglo que no acepta duplicados
+        ArrayList<Opcion>
+        
+        Set<Integer> enteros=new LinkedHashSet<>();
+        
+        while(enteros.size()<4)
+        {
+            Random x=new Random();
+            int valor=x.nextInt(4);
+            enteros.add(valor);
+        }
+        
+        // Este For necesita cambios
+        for(Integer i:enteros)
+        {
+            System.out.println(i);
+        }
+        
+        for(Integer i:enteros)
+        {
+            opcionesAleatorias.add(opciones.get(i))
+        }
+        
+    }
+}
---
+++ ProyectoFinal/src/modelo/Prueba.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author T-101
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<Opcion> opciones = PersistenciaPreguntas.leer().get(0).getOpciones();
+            for (Opcion o : opciones) {
+                System.out.println(o.getTitulo());
+            }
+            System.out.println("_____________");
+            PersistenciaPreguntas.opcionesAleatorias(opciones);
+        } catch (Exception ex) {
+            Logger.getLogger(Prueba.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
